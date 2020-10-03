ca78de476670a0b59947b948e53d1e034453e28b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2020-10-03 21:23:11 +0300

    Some compatibility with default align/size fields

--- src/BlockStyleWrapper/Edit.jsx
+++ src/BlockStyleWrapper/Edit.jsx
@@ -6,11 +6,17 @@ export default (props) => {
   return (
     <StyleWrapperEdit
       {...props}
-      data={data?.styles || {}}
+      data={{
+        ...data?.styles,
+        ...(data.align ? { align: data.align } : {}),
+        ...(data.size ? { size: data.size } : {}),
+      }}
       choices={[]}
       onChangeValue={(id, value) =>
         onChangeBlock(block, {
           ...data,
+          ...(id === 'align' ? { align: value } : {}),
+          ...(id === 'size' ? { size: value } : {}),
           styles: {
             ...data?.styles,
             [id]: value,
