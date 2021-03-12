e5aacef3426d8ed2d3397863bf51f53d883ec09c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-03-12 09:49:22 +0200

    Move palette button to block style wrapper

--- src/StyleWrapper/StyleWrapperEdit.jsx
+++ src/StyleWrapper/StyleWrapperEdit.jsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import { Portal } from 'react-portal';
 import { doesNodeContainClick } from 'semantic-ui-react/dist/commonjs/lib';
 import { Icon } from '@plone/volto/components';
-import themeSVG from '@plone/volto/icons/theme.svg';
 import SidebarPopup from '../SidebarPopup/SidebarPopup';
 import InlineForm from '@plone/volto/components/manage/Form/InlineForm';
 import { StyleSchema } from './schema';
 import clearSVG from '@plone/volto/icons/clear.svg';
 
 const StyleWrapperEdit = (props) => {
-  const { children, selected, onChangeValue, data } = props;
-  const [isOpen, setIsOpen] = React.useState(false);
+  const { selected, onChangeValue, data, isVisible, setIsVisible } = props;
+
   const nodeRef = React.useRef();
+  const schema = React.useMemo(() => StyleSchema(), []);
 
   const closeSidebar = React.useCallback(
     (e) => {
-      if (isOpen && !doesNodeContainClick(nodeRef.current, e)) setIsOpen(false);
+      if (isVisible && !doesNodeContainClick(nodeRef.current, e))
+        setIsVisible(false);
     },
-    [isOpen],
+    [isVisible, setIsVisible],
   );
 
   React.useEffect(() => {
@@ -27,63 +27,31 @@ const StyleWrapperEdit = (props) => {
     };
   }, [closeSidebar]);
 
-  const schema = React.useMemo(() => StyleSchema(), []);
-
-  return (
-    <>
-      {selected ? (
-        <>
-          <Portal
-            node={
-              __CLIENT__ &&
-              document.querySelector(
-                '#sidebar > .sidebar-container > .tabs-wrapper > .formtabs',
-              )
-            }
-          >
-            <div id="open-styles-button">
-              <button
-                onClick={() => {
-                  setIsOpen(true);
-                }}
-                title={`${
-                  props.type
-                    ? 'Style palette for ' + props.type
-                    : 'Style pallete'
-                }`}
-              >
-                <Icon name={themeSVG} size="18px" />
-              </button>
-            </div>
-          </Portal>
-          <SidebarPopup open={isOpen} ref={nodeRef}>
-            <InlineForm
-              schema={schema}
-              title={
-                <>
-                  {schema.title}
-                  <button
-                    onClick={() => {
-                      setIsOpen(false);
-                    }}
-                    style={{ float: 'right' }}
-                  >
-                    <Icon name={clearSVG} size="24px" />
-                  </button>
-                </>
-              }
-              onChangeField={(id, value) => {
-                onChangeValue(id, value);
+  return selected && isVisible ? (
+    <SidebarPopup open={isVisible} ref={nodeRef}>
+      <InlineForm
+        schema={schema}
+        title={
+          <>
+            {schema.title}
+            <button
+              onClick={() => {
+                setIsVisible(false);
               }}
-              formData={data}
-            />
-          </SidebarPopup>
-        </>
-      ) : (
-        ''
-      )}
-      {children}
-    </>
+              style={{ float: 'right' }}
+            >
+              <Icon name={clearSVG} size="24px" />
+            </button>
+          </>
+        }
+        onChangeField={(id, value) => {
+          onChangeValue(id, value);
+        }}
+        formData={data}
+      />
+    </SidebarPopup>
+  ) : (
+    ''
   );
 };
 
