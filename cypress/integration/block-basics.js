ed8dc6f55f1f48cb84f6c9879eb4a728e1308639
Daniela Mormocea <daniela.mormocea@gmail.com>
2021-12-16 23:27:42 +0200

    added cy tests

--- cypress/integration/block-basics.js
+++ cypress/integration/block-basics.js
@@ -22,6 +22,39 @@ describe('Blocks Tests', () => {
     cy.get('.blocks-chooser .title').contains('Media').click();
     cy.get('.content.active.media .button.image').contains('Image').click();
 
+    cy.get('[contenteditable=true]').first().type('{enter}');
+
+    //add text block
+    cy.get('.ui.basic.icon.button.block-add-button').first().click();
+    cy.get('.blocks-chooser .title').contains('Text').click();
+    cy.get('.content.active.text .button.text').contains('Text').click();
+    cy.get('.block-editor-text').last().type("This is a sample text");
+
+    cy.get('.open-styles-button button').click();
+    cy.get('.accordion.ui.fluid.styled').contains('Standard').click();
+    cy.get('.align-tools').first().click();
+    cy.get('#field-fontSize').click();
+    cy.get('.react-select__menu').contains('medium').click();
+    cy.get('.field-wrapper-align button').eq(1).click();
+    cy.get('.field-wrapper-stretch button').eq(1).click();
+    cy.get('.field-wrapper-size button').first().click();
+
+
+    cy.get('.accordion.ui.fluid.styled').contains('Decorations').click();
+    cy.get('.simple-color-picker-widget button').first().click();
+    cy.get('.github-picker.color-picker span').eq(4).click();
+    cy.wait(1000);
+    cy.get('.simple-color-picker-widget button').eq(2).click();
+    cy.get('.github-picker.color-picker span').eq(8).click();
+    cy.wait(1000);
+    cy.get('.simple-color-picker-widget button').eq(3).click();
+    cy.get('.simple-color-picker-widget button').eq(4).click();
+    cy.get('.github-picker.color-picker span').eq(8).click();
+
+
+    cy.get('.slider-widget-wrapper .slider-knob').first().trigger('mousedown', { which: 1 }, { force: true }).trigger('mousemove', 70, 0, {force: true}).trigger('mouseup');
+    cy.get('.slider-widget-wrapper .slider-knob').eq(1).trigger('mousedown', { which: 1 }, { force: true }).trigger('mousemove', 70, 0, {force: true}).trigger('mouseup');
+
     // Save
     cy.get('#toolbar-save').click();
     cy.url().should('eq', Cypress.config().baseUrl + '/cypress/my-page');
