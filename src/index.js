f27b69d8f9687b212403b7d49173edb8c2bfac69
Miu Razvan <miu.razvan28@gmail.com>
2022-08-04 10:35:55 +0300

    Use colors from available_colors

--- src/index.js
+++ src/index.js
@@ -84,6 +84,41 @@ const applyConfig = (config) => {
     config.settings.layoutOnlyBlockStyles = false;
   }
 
+  config.settings.available_colors = [
+    '#bbdbec',
+    '#9dc6d4',
+    '#5a93aa',
+    '#005d7b',
+    '#003d53',
+    '#ebefc6',
+    '#bdd494',
+    '#6bb535',
+    '#1e8339',
+    '#025e37',
+    '#464b0b',
+    '#b5c234',
+    '#777b1a',
+    '#f4f1bc',
+    '#e1e070',
+    '#0070ae',
+    '#fce6dc',
+    '#f39a86',
+    '#e73d5c',
+    '#b92f47',
+    '#8e1206',
+    '#fff6a6',
+    '#ffe525',
+    '#f7a600',
+    '#b94b19',
+    '#8d4107',
+    '#000000',
+    '#6f6f6e',
+    '#929291',
+    '#bcbcbc',
+    '#e3e3e3',
+    '#ffffff',
+  ];
+
   return config;
 };
 
